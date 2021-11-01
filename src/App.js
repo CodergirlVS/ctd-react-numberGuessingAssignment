af131981176f7cda7a47a37db71ce137b5bfe9b3
CodergirlVS <vaidehis1214@gmail.com>
2021-10-31 17:05:09 -0700

    Assignment Lesson 1.10 complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import NumberGuessingGame from "./NumberGuessingGame";
+import NumberGuessingGameOld from "./NumberGuessingGame";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NumberGuessingGame />
+        <NumberGuessingGameOld />
       </div>
     );
   }
